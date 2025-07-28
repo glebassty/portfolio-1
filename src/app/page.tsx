c0f70311a3f766fd22170404ae46be44ef363d2a
glebassty <glebassty@gmail.com>
2025-07-28 13:18:40 +0300

    add button styles without var color styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 // page.tsx
 
 import { Button } from "@/components/ui/Button";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export default function HomePage() {
   return (
     <div>
       <div className="bg-primary p-4 text-green-500">Привет!</div>
       <div className="text-primary">Привет!</div>
-      <Button>Нажми меня</Button>
+      <Button
+        icon={<PlusIcon className="h-5 w-5 text-white" />}
+        iconPosition="right"
+      >
+        Hire me
+      </Button>
     </div>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,22 +1,37 @@
 // components/ui/button.tsx
-import { type ButtonHTMLAttributes, type DetailedHTMLProps } from "react";
+import {
+  type ButtonHTMLAttributes,
+  type DetailedHTMLProps,
+  type ReactNode,
+} from "react";
 import { cn } from "@/lib/utils";
 
 type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
->;
+> & {
+  icon?: ReactNode;
+  iconPosition?: "left" | "right";
+};
 
-export function Button({ children, className, ...props }: ButtonProps) {
+export function Button({
+  children,
+  icon,
+  iconPosition = "right",
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <button
       className={cn(
-        "bg-primary hover:bg-primary-hover rounded-lg px-6 py-3 text-sm font-medium text-white shadow-md transition",
+        "flex items-center justify-center gap-3 rounded-lg border border-neutral-900 bg-orange-600 px-5 py-2.5 text-base font-medium text-white shadow-lg transition-colors duration-200 hover:bg-orange-700",
         className,
       )}
       {...props}
     >
+      {iconPosition === "left" && icon}
       {children}
+      {iconPosition === "right" && icon}
     </button>
   );
 }
