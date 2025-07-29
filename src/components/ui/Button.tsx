185fbe9dd30cece53878bf4cc496b1fd100e086b
glebassty <glebassty@gmail.com>
2025-07-29 14:33:48 +0300

    настройка цветов

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -22,7 +22,7 @@ export function Button({
   return (
     <button
       className={cn(
-        "font-myFont bg-accent border-secondary text-text-primary flex items-center rounded-lg border-[1px]",
+        "bg-secondary border-primary text-text-primary flex items-center rounded-lg border-[1px]",
         className,
       )}
       {...props}
@@ -30,7 +30,7 @@ export function Button({
       <p className="py-2.5 pr-1.5 pl-2.5 text-xs sm:py-3 sm:pr-2.5 sm:pl-2 sm:text-base">
         {children}
       </p>
-      <div className="bg-secondary w-[1px] self-stretch" />
+      <div className="bg-primary w-[1px] self-stretch" />
       <div className="py-3 pr-2.5 pl-2 sm:py-3.5 sm:pr-3 sm:pl-2">
         <Icon className="h-2.5 w-2.5 sm:h-3.5 sm:w-3.5" />
       </div>
