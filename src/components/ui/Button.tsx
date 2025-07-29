d48c0c57a824fe23f34e0d59cc8c2ad20ab21948
glebassty <glebassty@gmail.com>
2025-07-29 11:28:20 +0300

    вот мобилка для кнопки

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -27,12 +27,12 @@ export function Button({
       )}
       {...props}
     >
-      <p className="py-3 pr-2.5 pl-2 text-base sm:py-2.5 sm:pr-1.5 sm:pl-2.5 sm:text-xs">
+      <p className="py-2.5 pr-1.5 pl-2.5 text-xs sm:py-3 sm:pr-2.5 sm:pl-2 sm:text-base">
         {children}
       </p>
       <div className="bg-secondary w-[1px] self-stretch" />
-      <div className="py-3.5 pr-3 pl-2 sm:py-3 sm:pr-2.5 sm:pl-2">
-        <Icon className="h-3.5 w-3.5 sm:h-2 sm:w-2" />
+      <div className="py-3 pr-2.5 pl-2 sm:py-3.5 sm:pr-3 sm:pl-2">
+        <Icon className="h-2.5 w-2.5 sm:h-3.5 sm:w-3.5" />
       </div>
     </button>
   );
