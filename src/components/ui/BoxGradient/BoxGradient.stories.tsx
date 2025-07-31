10fcedbbd79c3b328f7dc1c43ed23bb8b6357e39
glebassty <glebassty@gmail.com>
2025-07-31 12:35:35 +0300

    работа над градиентом

---
+++ src/components/ui/BoxGradient/BoxGradient.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import { BoxGradient } from "./BoxGradient";
+
+const meta: Meta<typeof BoxGradient> = {
+  title: "ui/BoxGradient",
+  component: BoxGradient,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BoxGradient>;
+
+export const WithBorderGradient: Story = {
+  args: {
+    children: (
+      <span className="text-lg font-medium text-white">Product design</span>
+    ),
+    className: "w-[300px] h-[100px] flex items-center justify-center",
+    angle: 249.05,
+    from: "#E63E21",
+    to: "rgba(230, 62, 33, 0)",
+    fallbackColor: "#1B1B1B",
+    borderFrom: "#E63E21",
+    borderTo: "#262626",
+    borderGradient: true,
+  },
+};
