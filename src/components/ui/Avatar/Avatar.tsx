a076e2177c0e2248091c64d076717d28b09f0bc4
glebassty <glebassty@gmail.com>
2025-08-01 16:14:33 +0300

    сделал аватар вроде

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -8,12 +8,14 @@ export const Avatar = ({ className, ...props }: AvatarProps) => {
   return (
     <div
       className={cn(
-        "border-border-avatar bg-bg-avatar relative h-44.5 w-44.5 rounded-full border-2 p-2.5",
+        "border-border-avatar b bg-bg-avatar relative h-44.5 w-44.5 rounded-full border-2 p-2.5",
         className,
       )}
       {...props}
     >
-      <Image src="/img/avatar.png" fill alt="Avatar" />
+      <div className="relative h-full w-full overflow-hidden rounded-full">
+        <Image src="/img/avatar.png" fill alt="Avatar" />
+      </div>
     </div>
   );
 };
